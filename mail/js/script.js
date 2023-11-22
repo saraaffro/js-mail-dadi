255b70bd67de3b033ea735aed9ea2a213d84979f
saraaffro <saraaffro@gmail.com>
2023-11-22 14:25:23 +0100

    fine mail input

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,13 +1,19 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 
+// seleziono input
+const inputMail = document.getElementById("mail");
+const sendButton = document.getElementById("sendbutton");
+
+
 // creare lista email che possono accedere
 const mailList = ["mariorossi.gmail.com", "nomecognome.gmail.com", "azienda.info.it", "benvenuto.gmail.com", "buongiorno.gmail.com", "ciao.gmail.com"];
 // non si può mettere @
 console.log(mailList);
 
+
 // chiedere all'utente la sua mail
-const userMail = prompt("inserisci la tua email");
+const userMail = inputMail.value;
 console.log(userMail);
 
 let text;
@@ -17,14 +23,20 @@ for (i = 0; i < mailList.length; i++){
 
     let mailElement = mailList[i];
 
+     // funzionamento click
+     sendButton.addEventListener("click",
+
+     function(){
+         document.getElementById("description").innerHTML = text;
+     }
+     );
+
     if(userMail === mailElement){
         text = "La tua mail risulta in quelle registrate, bentornato, puoi accedere a questo sito";
-        console.log(text);
     }
     else{
         text = "La tua mail non corrisponde a quelle cha hanno accesso al sito, perciò non puoi accedervi";
     }
-
-    
-
 }
+
+
