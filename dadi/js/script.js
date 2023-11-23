3565aa603268c9f251e819a47eeebd3a1d8ef502
saraaffro <saraaffro@gmail.com>
2023-11-23 10:10:08 +0100

    fix: random number user

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,7 +1,7 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che “cosa” ci serve?
 
 // Generare un numero random da 1 a 6 per l'utente
-const userNumber = prompt("Inserisci un numero a caso tra 1 e 6");
+const userNumber = Math.floor(Math.random() * 6) +1;
 console.log(userNumber);
 
 // Generare un numero random da 1 a 6 per il computer
@@ -16,7 +16,7 @@ if(userNumber > pcNumber){
 } else if (userNumber < pcNumber){
     text = "Il tuo numero è minore, hai perso :(";
 } else{
-    text = "Il tuo numero è uguale a quello del computer, siete pari ;)";
+    text = "I vostri numeri sono uguali, siete pari ;)";
 }
 
 console.log(text);
